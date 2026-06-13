e5827544c2fc5ebb11cfd0b235c31772c6901937
shaolonger <shaolong.chuang@gmail.com>
2026-06-13 07:09:11 -0700

    Add batch asset edit API

--- cmd/server.go
+++ cmd/server.go
@@ -270,6 +270,7 @@ func RunServer() {
 			clientGroup.POST("/add", admin.AddClient)
 			clientGroup.GET("/list", admin.ListClients)
 			clientGroup.GET("/asset-summary", admin.GetClientAssetSummary)
+			clientGroup.POST("/batch-edit", admin.BatchEditClientAssets)
 			clientGroup.GET("/:uuid", admin.GetClient)
 			clientGroup.POST("/:uuid/edit", admin.EditClient)
 			clientGroup.POST("/:uuid/remove", admin.RemoveClient)
